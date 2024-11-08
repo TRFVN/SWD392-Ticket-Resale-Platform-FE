7f692d8d415bfe8a045ab946e99bce2b3ee51aec
Khoa <khoaho120703@gmail.com>
2024-11-08 16:02:46 +0700

    [Cretae]_Static Management of Staff

---
+++ src/pages/Staff/Static/Data.js
@@ -0,0 +1,74 @@
+export const transactionData = [
+  {
+    month: "January",
+    totalTransactions: 1200,
+    successfulTransactions: 950,
+    failedTransactions: 250,
+  },
+  {
+    month: "February",
+    totalTransactions: 1100,
+    successfulTransactions: 870,
+    failedTransactions: 230,
+  },
+  {
+    month: "March",
+    totalTransactions: 1300,
+    successfulTransactions: 980,
+    failedTransactions: 320,
+  },
+  {
+    month: "April",
+    totalTransactions: 1400,
+    successfulTransactions: 1150,
+    failedTransactions: 250,
+  },
+  {
+    month: "May",
+    totalTransactions: 1500,
+    successfulTransactions: 1200,
+    failedTransactions: 300,
+  },
+  {
+    month: "June",
+    totalTransactions: 1600,
+    successfulTransactions: 1280,
+    failedTransactions: 320,
+  },
+  {
+    month: "July",
+    totalTransactions: 1700,
+    successfulTransactions: 1350,
+    failedTransactions: 350,
+  },
+  {
+    month: "August",
+    totalTransactions: 1800,
+    successfulTransactions: 1450,
+    failedTransactions: 350,
+  },
+  {
+    month: "September",
+    totalTransactions: 1750,
+    successfulTransactions: 1400,
+    failedTransactions: 350,
+  },
+  {
+    month: "October",
+    totalTransactions: 1850,
+    successfulTransactions: 1500,
+    failedTransactions: 350,
+  },
+  {
+    month: "November",
+    totalTransactions: 1900,
+    successfulTransactions: 1550,
+    failedTransactions: 350,
+  },
+  {
+    month: "December",
+    totalTransactions: 2000,
+    successfulTransactions: 1600,
+    failedTransactions: 400,
+  },
+];
--- src/pages/Staff/Static/index.jsx
+++ src/pages/Staff/Static/index.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import { ChartColumnBig } from "lucide-react";
+import { transactionData } from "./Data";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 function StaticManagement() {
   return (
-    <div className="flex flex-col justify-start items-start w-full p-8">
+    <div className="flex flex-col justify-start items-start gap-8 w-full p-8">
       <div className="flex flex-row justify-start items-center gap-3 ">
         <ChartColumnBig />
         <span className="text-2xl font-semibold">Static Mangement</span>
+      </div>
+      <div className="flex flex-col justify-start items-start w-full gap-8 border rounded-md bg-white p-8">
+        <div className="text-xl font-semibold">Transaction in 2023</div>
+        <BarChart
+          width={1450}
+          height={250}
+          data={transactionData}
+          className="w-full"
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="failedTransactions" fill="#e30b0b" />
+          <Bar dataKey="successfulTransactions" fill="#09e02d" />
+          <Bar dataKey="totalTransactions" fill="#9b1adb" />
+        </BarChart>
       </div>
     </div>
   );
