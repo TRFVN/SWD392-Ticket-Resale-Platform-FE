c2b97ea5f775158b429997e1527cd69bd8df5447
Truongvbn <vobanhattruong2@gmail.com>
2024-11-08 13:03:04 +0700

    add to cart-delete

--- src/components/ticket/TicketCard.jsx
+++ src/components/ticket/TicketCard.jsx
@@ -144,6 +144,7 @@ const TicketCard = ({ ticket, onAddToCart }) => {
               {ticket.serialNumber}
             </span>
           </div>
+
           {/* Action Buttons
           <div className="flex items-center gap-2 ml-auto">
             <button
